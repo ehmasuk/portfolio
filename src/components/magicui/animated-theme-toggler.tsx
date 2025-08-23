e81d6d4bbc4b8a99435b8dbc4663a9f63d7a122b
ehmasuk <ehmasuk@gmail.com>
2025-08-24 01:19:07 +0600

    theme adjust

--- src/components/magicui/animated-theme-toggler.tsx
+++ src/components/magicui/animated-theme-toggler.tsx
@@ -1,16 +1,18 @@
 "use client";
 
-import { SunDim } from "lucide-react";
-import { useState, useRef } from "react";
-import { flushSync } from "react-dom";
 import { cn } from "@/lib/utils";
 import { MoonIcon } from "@radix-ui/react-icons";
+import { SunDim } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useRef, useState } from "react";
+import { flushSync } from "react-dom";
 
 type props = {
   className?: string;
 };
 
 export const AnimatedThemeToggler = ({ className }: props) => {
+  const { setTheme } = useTheme();
   const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
   const buttonRef = useRef<HTMLButtonElement | null>(null);
   const changeTheme = async () => {
@@ -20,11 +22,11 @@ export const AnimatedThemeToggler = ({ className }: props) => {
       flushSync(() => {
         const dark = document.documentElement.classList.toggle("dark");
         setIsDarkMode(dark);
+        setTheme(dark ? "dark" : "light");
       });
     }).ready;
 
-    const { top, left, width, height } =
-      buttonRef.current.getBoundingClientRect();
+    const { top, left, width, height } = buttonRef.current.getBoundingClientRect();
     const y = top + height / 2;
     const x = left + width / 2;
 
@@ -34,16 +36,13 @@ export const AnimatedThemeToggler = ({ className }: props) => {
 
     document.documentElement.animate(
       {
-        clipPath: [
-          `circle(0px at ${x}px ${y}px)`,
-          `circle(${maxRad}px at ${x}px ${y}px)`,
-        ],
+        clipPath: [`circle(0px at ${x}px ${y}px)`, `circle(${maxRad}px at ${x}px ${y}px)`],
       },
       {
         duration: 700,
         easing: "ease-in-out",
         pseudoElement: "::view-transition-new(root)",
-      },
+      }
     );
   };
   return (
