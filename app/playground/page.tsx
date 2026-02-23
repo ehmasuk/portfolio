6503a47cae3bcfa68548e61cbe23f6bb0430f364
ehmasuk <ehmasuk@gmail.com>
2026-02-24 01:46:09 +0600

    fix: linting

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,10 +1,10 @@
 import GridSection from "@/components/global/grid-section";
-import AddUser from "@/data/ui/real-time-presence";
+
 
 function page() {
   return (
     <GridSection>
-      <AddUser />
+      hi
     </GridSection>
   );
 }
